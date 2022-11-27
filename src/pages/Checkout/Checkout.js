1cc82096e22831f1290b13ca8b1ff2c38f88c3c1
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2022-11-27 21:23:47 +0700

    update UI Checkout page => 90%

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -3,6 +3,7 @@ import classNames from "classnames/bind";
 
 import styles from './Checkout.module.scss';
 import Product from "../../components/Product/Product";
+import ProductCard from '../../layouts/components/Cart/ProductCard';
 import Title from '../../components/Title/Title';
 import Bill from '../../layouts/components/Bill/Bill';
 import AddressCard from '../../layouts/components/AddressCard/AddressCard';
@@ -12,6 +13,7 @@ const cx = classNames.bind(styles)
 function Checkout() {
 
   const [address, setAddress] = useState([])
+  const [productsCategory, setProductsCategory] = useState([])
   const [products, setProducts] = useState([])
 
   const email = localStorage.getItem('email').replace('@', '%40')
@@ -21,10 +23,18 @@ function Checkout() {
           .then((content) => {
               setAddress(content)
           });
-  }, [])
+  }, [email])
 
   useEffect(() => {
       fetch('http://localhost:3001/products/MDM04')
+          .then((response) => response.json())
+          .then((dataCategory) => {
+              setProductsCategory(dataCategory)
+          });
+  }, [])
+
+  useEffect(() => {
+      fetch('http://localhost:3001/products')
           .then((response) => response.json())
           .then((data) => {
               setProducts(data)
@@ -33,20 +43,54 @@ function Checkout() {
 
   const addressArr = address.slice(0, address.length)
 
-  const productsArr_1 = products.slice(0, 3)
-  const productsArr_2 = products.slice(3, 6)
+  const productsArr_1 = productsCategory.slice(0, 3)
+  const productsArr_2 = productsCategory.slice(3, 6)
+
+  const ids = products.map(product => { // lấy ra mảng các id từ products => dùng tính counts
+    return product.maMonAn;
+  })
+  const counts = ids.map(id => { // lấy ra mảng các thông tin products đc add to cart
+    if (id !== null) {
+      return JSON.parse(localStorage.getItem(`gioHang${id}`))
+    }
+  })
+  const productsInCart = []
+  counts.map(count => {
+    if (count !== null) {
+      productsInCart.push(count)
+    }
+  })
 
   return (
     <div className={cx('wrapper')}>
 
       <div className={cx('left-content')}>
+        <Title content={'GIỎ HÀNG CỦA BẠN'}/>
+        <div className={cx('products-in-cart')}>
+          {productsInCart 
+            ? 
+            <>
+              {
+                productsInCart.map((data) => (
+                  <ProductCard key={data.maMonAn} data={data}/>
+                ))
+              }
+            </>
+            :
+            <>
+              <p>Bạn chưa thêm món nào vào giỏ cả!!</p>
+            </>
+          }
+        </div>
+        
+        <br />
         <Title content={'NGON HƠN KHI ĂN KÈM'}/>
         {productsArr_1 !== null
             ?
             <>
               <div className={cx('products')}>
-                {productsArr_1.map((data) => (
-                  <Product key={data.maMonAn} data={data}/>
+                {productsArr_1.map((dataCategory) => (
+                  <Product key={dataCategory.maMonAn} data={dataCategory}/>
                 ))} 
               </div>
             </>
@@ -56,8 +100,8 @@ function Checkout() {
             ?
             <>
               <div className={cx('products')}>
-                {productsArr_2.map((data) => (
-                  <Product key={data.maMonAn} data={data}/>
+                {productsArr_2.map((dataCategory) => (
+                  <Product key={dataCategory.maMonAn} data={dataCategory}/>
                 ))} 
               </div>
             </>
